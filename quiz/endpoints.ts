6a8503bbde54b81429b2b630296d4f20948f0f3d
Francis James Tolentino <francistolentino1107@gmail.com>
2023-10-30 21:56:31 +0800

    backend url in env

--- quiz/endpoints.ts
+++ quiz/endpoints.ts
@@ -1,5 +1,11 @@
 
-export const QUIZ_BASE_API = "http://localhost:8000/api/quiz/";
-export const CREATE_QUIZ_API = "http:/dd/localhost:8000/api/quiz/create/";
-export const SUBMIT_ANSWERS_API = "http://localhost:8000/api/quiz/evaluate/";
-export const GET_QUIZZES_API = "http://localhost:8000/api/";
+export const QUIZ_BASE_API = `${process.env.BACKEND_URL}/api/quiz/`;
+export const CREATE_QUIZ_API = `${process.env.BACKEND_URL}/api/quiz/create/`;
+export const SUBMIT_ANSWERS_API = `${process.env.BACKEND_URL}/api/quiz/evaluate/`;
+export const GET_QUIZZES_API = `${process.env.BACKEND_URL}/api/`;
+
+
+// export const QUIZ_BASE_API = "http://localhost:8000/api/quiz/";
+// export const CREATE_QUIZ_API = "http://localhost:8000/api/quiz/create/";
+// export const SUBMIT_ANSWERS_API = "http://localhost:8000/api/quiz/evaluate/";
+// export const GET_QUIZZES_API = "http://localhost:8000/api/";
--- quiz/next.config.js
+++ quiz/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL
+  }
 }
 
 module.exports = nextConfig
